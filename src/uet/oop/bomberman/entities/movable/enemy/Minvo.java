290bbf46d75931d201682eb170e0bca44642eb40
LeHuyBinh <laxus.predator@gmail.com>
2021-11-21 23:19:38 +0700

    Update sound

---
+++ src/uet/oop/bomberman/entities/movable/enemy/Minvo.java
@@ -0,0 +1,15 @@
+package uet.oop.bomberman.entities.movable.enemy;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.entities.Entity;
+
+public class Minvo extends Entity {
+    public Minvo(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
